4f7c15c9b64a1ee7d4be25a036ce80a3e7294a71
CullenMar <cmarchenko@csumb.edu>
2017-11-02 17:27:26 -0700

    Put rest of currently completed interfaces into this commit

---
+++ app/src/main/java/edu/csumb/ma6317/myapplication/incomingCall.java
@@ -0,0 +1,13 @@
+package edu.csumb.ma6317.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class incomingCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incoming_call);
+    }
+}
