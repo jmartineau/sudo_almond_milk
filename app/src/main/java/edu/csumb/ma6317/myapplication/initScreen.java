6b5cb0c83194649ee78db4b139411f52befdff7b
jmartineau <jmartineau@csumb.edu>
2017-11-12 15:36:33 -0800

    Added login support for email and Google

--- app/src/main/java/edu/csumb/ma6317/myapplication/initScreen.java
+++ app/src/main/java/edu/csumb/ma6317/myapplication/initScreen.java
@@ -2,12 +2,65 @@ package edu.csumb.ma6317.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class initScreen extends AppCompatActivity {
+
+    // FirebaseUI sign-in code
+    public static final int RC_SIGN_IN = 1;
+
+    // Firebase instance variables
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_screen);
+
+        // Initialize Firebase components
+        auth = FirebaseAuth.getInstance();
+
+        // Handles user sign-in
+        final Button signInButton = findViewById(R.id.signInButt);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if (auth.getCurrentUser() != null) {
+                    // User is signed in
+                } else {
+                    // User is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                          new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        });
+
+        final Button logoutButton = findViewById(R.id.logoutButt);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                signOut();
+            }
+        });
+
+
+    }
+
+    public void signOut() {
+        AuthUI.getInstance().signOut(this);
     }
 }
