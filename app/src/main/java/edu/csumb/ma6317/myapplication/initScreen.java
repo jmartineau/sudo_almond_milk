ef603dd13c80f2483d7f22cb629fe9bd7724a0bd
Mavey Ma <maveyma@gmail.com>
2017-11-12 14:38:39 -0800

    initScreen has onClick to either signIn / contGuest

--- app/src/main/java/edu/csumb/ma6317/myapplication/initScreen.java
+++ app/src/main/java/edu/csumb/ma6317/myapplication/initScreen.java
@@ -1,13 +1,42 @@
 package edu.csumb.ma6317.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class initScreen extends AppCompatActivity {
+public class initScreen extends AppCompatActivity implements OnClickListener {
+
+    private Button signInButton;
+    private Button contGuestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_screen);
+
+        //get references to the widgets in activity_init_screen.xml
+        signInButton = (Button) findViewById(R.id.signInButt);
+        contGuestButton = (Button) findViewById(R.id.guestButt);
+
+        //set the click listeners for the buttons
+        signInButton.setOnClickListener(this);
+        contGuestButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.signInButt) {
+            Intent intent = new Intent(this, signUp.class);
+            startActivity(intent);
+        }
+        else if (v.getId() == R.id.guestButt) {
+            Intent intent = new Intent(this, profCreate.class);
+            startActivity(intent);
+        }
+        else {
+            return;
+        }
     }
 }
