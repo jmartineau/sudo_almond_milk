8f17548f68f6d01af32fb03a790af09ee3cdeed9
Noe Lomeli <nolomeli@csumb.edu>
2017-11-15 15:34:09 -0800

    Continue button after login goes to MapsActivity

--- app/src/main/java/edu/csumb/ma6317/myapplication/profCreate.java
+++ app/src/main/java/edu/csumb/ma6317/myapplication/profCreate.java
@@ -1,13 +1,31 @@
 package edu.csumb.ma6317.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class profCreate extends AppCompatActivity {
+public class profCreate extends AppCompatActivity implements View.OnClickListener {
+
+    // Button variables
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prof_create);
+
+        continueButton = findViewById(R.id.submitButt);
+        continueButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.submitButt) {
+            // User is signed in
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        }
     }
 }
+
